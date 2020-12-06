0ab82487c37667a5ecbbc3f7238be728022252cf
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2020-12-06 13:22:01 -0600

    added Airtable API and minified returns

---
+++ pages/api/getTodos.js
@@ -0,0 +1,26 @@
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+const table = base(process.env.AIRTABLE_TABLE_NAME);
+
+
+const minifyRecords = (records) => {
+  return records.map(record => getMinifiedRecords(record))
+}
+
+const getMinifiedRecords = (record) => {
+  if (!record.fields.completed){
+      record.fields.completed = false;
+  }
+  return {
+    id: record.id,
+    fields: record.fields
+  }
+}
+
+export default async (req, res) => {
+    const records = await table.select({}).firstPage();
+    const minifiedRecords = minifyRecords(records);
+    res.statusCode = 200
+    res.json(minifiedRecords);
+  };
