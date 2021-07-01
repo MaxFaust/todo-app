e40032d5c7e7aedb3557dd5f0f8ceb822c845c47
NICHOLAS FAUST <nicholasfaust@NICHOLASs-MacBook-Air.local>
2021-07-01 09:19:47 -0500

    undo callback redirectTo

--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,10 +1,10 @@
-import auth0 from './utils/auth0';
+import auth0 from "./utils/auth0"
 
 export default async function callback(req, res) {
-  try {
-    await auth0.handleCallback(req, res, { redirectTo: '/api/login' });
-  } catch (error) {
-    console.error(error);
-    res.status(error.status || 400).end(error.message);
-  }
+	try {
+		await auth0.handleCallback(req, res, { redirectTo: "/" })
+	} catch (error) {
+		console.error(error)
+		res.status(error.status || 400).end(error.message)
+	}
 }
